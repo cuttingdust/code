dc484db01a758a70f35844cc11a348ec2935c8b6
cuttingDust_47 <18862963156@163.com>
2023-02-04 00:32:21 +0800

    UI联动

---
+++ QtOpengl/04_QTUI/helloopenglwidget.h
@@ -0,0 +1,40 @@
+#ifndef HELLOOPENGLWIDGET_H
+#define HELLOOPENGLWIDGET_H
+
+#include <QWidget>
+
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions>
+
+class HelloOpenGLWidget : public QOpenGLWidget, protected QOpenGLFunctions
+{
+    Q_OBJECT
+public:
+    enum Shape {
+        None,
+        Rect,
+        Circle,
+        Triangle
+    };
+    
+    explicit HelloOpenGLWidget(QWidget *parent = nullptr);
+    
+    ~HelloOpenGLWidget();
+    
+    void drawShape(Shape shape);
+    
+    void setWireframe(bool wireframe);
+    
+protected:
+    void initializeGL() override;
+    void resizeGL(int w, int h) override;
+    void paintGL() override;
+    
+signals:
+    
+private:
+    Shape shape_;
+    
+};
+
+#endif // HELLOOPENGLWIDGET_H
