e5f12db28a63f21a2fc1adfdbe119ae560649f56
cuttingDust_47 <18862963156@163.com>
2023-03-13 23:27:55 +0800

    模版测试UI显示

---
+++ QtOpengl/21_01_DepthTest/camera.h
@@ -0,0 +1,78 @@
+#ifndef __CAMERA_H__
+#define __CAMERA_H__
+
+#include<QMatrix4x4>
+
+#include <vector>
+#include <QtGui/QtGui>
+
+// 移动方向枚举量. 是一种抽象，以避开特定于窗口系统的输入方法
+// 我们这里是WSAD
+enum Camera_Movement {
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+};
+
+// 默认值
+const float YAW         = -90.0f;
+const float PITCH       =  0.0f;
+const float SPEED       =  3.5f;
+const float SENSITIVITY =  0.1f;
+const float ZOOM        =  45.0f;
+
+// 一个抽象的camera类，用于处理输入并计算相应的Euler角度、向量和矩阵，以便在OpenGL中使用
+class Camera
+{
+public:
+    // constructor with vectors
+    explicit Camera(QVector3D position = QVector3D(0.0f, 0.0f, 0.0f), QVector3D up = QVector3D(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH);
+
+    // constructor with scalar values
+    explicit Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch);
+
+public:
+    void setPosition(const QVector3D &position);
+    const QVector3D &getPosition() const;
+
+    float getZoom() const;
+    void setZoom(float zoom);
+
+    const QVector3D &getFront() const;
+    void setFront(const QVector3D &front);
+
+public:
+    // returns the view matrix calculated using Euler Angles and the LookAt Matrix
+    QMatrix4x4 getViewMatrix() const;
+
+    // 处理从任何类似键盘的输入系统接收的输入。接受摄像机定义枚举形式的输入参数（从窗口系统中提取）
+    void processKeyboard(Camera_Movement direction, float deltaTime);
+
+    // 处理从鼠标输入系统接收的输入。需要x和y方向上的偏移值。
+    void processMouseMovement(float xoffset, float yoffset, bool constrainPitch = true);
+
+    // 处理从鼠标滚轮事件接收的输入。仅需要在垂直车轮轴上输入
+    void processMouseScroll(float yoffset);
+
+private:
+    // 根据相机的（更新的）Euler角度计算前矢量
+    void updateCameraVectors();
+private:
+    // camera Attributes
+    QVector3D position_;
+    QVector3D front_;
+    QVector3D up_;
+    QVector3D right_;
+    QVector3D worldUp_;
+    // euler Angles
+    float yaw_;
+    float pitch_;
+    // camera options
+    float movementSpeed_;
+    float mouseSensitivity_;
+    float zoom_;
+};
+#endif
---
+++ QtOpengl/21_01_DepthTest/modelloadingowgt.h
@@ -0,0 +1,96 @@
+#ifndef __MODELLOADINGOWGT_H__
+#define __MODELLOADINGOWGT_H__
+
+#include "environmentsettingdialog.h"
+#include "depthtestdialog.h"
+#include "camera.h"
+#include "mesh.h"
+#include "model.h"
+#include "lightmodel.h"
+#include "cubemodel.h"
+#include "planemodel.h"
+
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions_4_1_Core>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLTexture>
+
+#include <QWidget>
+#include <QTimer>
+#include <QTime>
+#include <QElapsedTimer>
+#include <QImage>
+
+class ModelLoadingOWgt : public QOpenGLWidget, protected  QOpenGLFunctions_4_1_Core
+{
+    Q_OBJECT
+public:
+    explicit ModelLoadingOWgt(QWidget *parent = nullptr);
+    ~ModelLoadingOWgt() noexcept;
+
+public:
+    void setWireframe(bool wireframe);
+    void setEnvSettingType(EnvironmentType type);
+    void loadModel(const std::string path);
+public:
+    EnvironmentType getViewEnvType() const;
+    void setViewEnvType(EnvironmentType viewEnvType);
+
+    DepthFuncType getTestFuncType() const;
+    void setTestFuncType(DepthFuncType testFuncType);
+
+    bool isBShowLights() const;
+    void setBShowLights(bool bShowLights);
+
+    bool isBDrawCube() const;
+    void setBDrawCube(bool bDrawCube);
+
+    bool isBDrawPlane() const;
+    void setBDrawPlane(bool bDrawPlane);
+
+protected:
+    void initializeGL() override;
+    void resizeGL(int w, int h) override;
+    void paintGL() override;
+    void keyPressEvent(QKeyEvent* event) override;
+    void mouseMoveEvent(QMouseEvent* event) override;
+    void wheelEvent(QWheelEvent* event) override;
+
+protected slots:
+    void slotUpdateTimer();
+    
+signals:
+
+private:
+    QVector3D cameraPosInit(float maxY, float minY);
+    void DrawShaderProgram(QMatrix4x4 projection, QMatrix4x4 view);
+private:
+    QOpenGLShaderProgram shaderProgram_;
+    QOpenGLShaderProgram lightShaderProgram_;
+    QTimer updateTimer_;
+    QElapsedTimer elapsedTimer_;
+    Camera camera_;
+
+    QVector3D clearColor_;
+    QVector3D dirlightAmbient_;
+    QVector3D dirlightDiffuse_;
+    QVector3D dirlightSpecular_;
+    EnvironmentType viewEnvType_;
+    DepthFuncType testFuncType_;
+    GLenum testFunc_;
+
+
+private:
+
+    Model* aModel_;
+    LightModel* aLight_;
+    CubeModel* aCube_;
+    PlaneModel* aPlane_;
+
+    bool bShowLights_;
+    bool bDrawCube_;
+    bool bDrawPlane_;
+
+};
+
+#endif
