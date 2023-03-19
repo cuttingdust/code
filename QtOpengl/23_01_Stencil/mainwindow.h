21716fbfc345413664aa5915a32eec437aef9aac
cuttingDust_47 <18862963156@163.com>
2023-03-20 00:46:47 +0800

    鼠标选中模型并且增加模型轮廓显示（模版测试）

---
+++ QtOpengl/23_01_Stencil/mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QVector3D>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+protected slots:
+    void slotWireframeView();
+    void slotEnvSetting();
+    void slotDepthTestSetting();
+    void slotLoadModel();
+    void slotShowLights();
+    void slotDrawCube();
+    void slotDrawPlane();
+    void slotZbufferShader();
+    void slotMousePickingPos(QVector3D pos);
+    void slotStenCil();
+
+private:
+    Ui::MainWindow *ui_;
+
+};
+#endif // MAINWINDOW_H
---
+++ QtOpengl/23_01_Stencil/modelloadingowgt.h
@@ -0,0 +1,125 @@
+#ifndef __MODELLOADINGOWGT_H__
+#define __MODELLOADINGOWGT_H__
+
+#include "environmentsettingdialog.h"
+#include "depthtestdialog.h"
+#include "camera.h"
+#include "mesh.h"
+#include "model.h"
+#include "lightmodel.h"
+#include "cubemodel.h"
+#include "planemodel.h"
+
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions_4_1_Core>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLTexture>
+
+#include <QWidget>
+#include <QTimer>
+#include <QTime>
+#include <QElapsedTimer>
+#include <QImage>
+#include <QVector3D>
+
+class ModelLoadingOWgt : public QOpenGLWidget, protected  QOpenGLFunctions_4_1_Core
+{
+    Q_OBJECT
+public:
+    explicit ModelLoadingOWgt(QWidget *parent = nullptr);
+    ~ModelLoadingOWgt() noexcept;
+
+    struct ModelInfo{
+        Model *model;
+        QVector3D worldPos;
+        float pitch;
+        float yaw;
+        float roll;
+        bool isSelected;
+        QString name;
+    };
+
+public:
+    void setWireframe(bool wireframe);
+    void setEnvSettingType(EnvironmentType type);
+    void loadModel(const std::string path);
+public:
+    EnvironmentType getViewEnvType() const;
+    void setViewEnvType(EnvironmentType viewEnvType);
+
+    DepthFuncType getTestFuncType() const;
+    void setTestFuncType(DepthFuncType testFuncType);
+
+    bool isBShowLights() const;
+    void setBShowLights(bool bShowLights);
+
+    bool isBDrawCube() const;
+    void setBDrawCube(bool bDrawCube);
+
+    bool isBDrawPlane() const;
+    void setBDrawPlane(bool bDrawPlane);
+
+    bool isBDepthTestShader() const;
+    void setBDepthTestShader(bool bDepthTestShader);
+
+    bool isBStencil() const;
+    void setBStencil(bool bStencil);
+
+protected:
+    void initializeGL() override;
+    void resizeGL(int w, int h) override;
+    void paintGL() override;
+    void keyPressEvent(QKeyEvent* event) override;
+    void mouseMoveEvent(QMouseEvent* event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseDoubleClickEvent(QMouseEvent* event) override;
+    void wheelEvent(QWheelEvent* event) override;
+
+protected slots:
+    void slotUpdateTimer();
+    
+signals:
+    void signalMousePickingPos(QVector3D worldPos);
+
+private:
+    QVector3D cameraPosInit();
+    void drawShaderProgram(QMatrix4x4 projection, QMatrix4x4 view);
+    void drawOutlineShaderProgram(QMatrix4x4 projection, QMatrix4x4 view);
+    QVector4D worldPosFromViewPort(int posX,int posY);
+private:
+    QOpenGLShaderProgram shaderProgram_;
+    QOpenGLShaderProgram lightShaderProgram_;
+    QOpenGLShaderProgram outlineShaderProgram_;
+    QTimer updateTimer_;
+    QElapsedTimer elapsedTimer_;
+    Camera camera_;
+
+    QVector3D clearColor_;
+    QVector3D dirlightAmbient_;
+    QVector3D dirlightDiffuse_;
+    QVector3D dirlightSpecular_;
+    EnvironmentType viewEnvType_;
+    DepthFuncType testFuncType_;
+    GLenum testFunc_;
+
+
+private:
+    QMap<QString, ModelInfo> modelsMap_;
+
+    LightModel* aLight_;
+    CubeModel* aCube_;
+    PlaneModel* aPlane_;
+
+    bool bShowLights_;
+    bool bDrawCube_;
+    bool bDrawPlane_;
+
+    bool bDepthTestShader_;
+    bool bStencil_;
+
+private:
+    bool bModelMoving_;
+
+};
+
+#endif
