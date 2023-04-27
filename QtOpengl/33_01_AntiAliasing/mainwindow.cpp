03c93443889e973ff8aa53159e3b1b45f09907db
cuttingDust_47 <18862963156@163.com>
2023-04-28 01:42:33 +0800

    增加多重采样抗拒尺功能（FramBuffer也增加了，多重采样：4）

---
+++ QtOpengl/33_01_AntiAliasing/mainwindow.cpp
@@ -0,0 +1,188 @@
+#include <QFileDialog>
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "depthtestdialog.h"
+#include "frambufferdialog.h"
+#include "shownormaldg.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui_(new Ui::MainWindow)
+{
+    ui_->setupUi(this);
+    setCentralWidget(ui_->openGLWidget);
+    connect(ui_->actionWireFrameView, SIGNAL(triggered()), this, SLOT(slotWireframeView()));
+    connect(ui_->actionEnvSetting, SIGNAL(triggered()), this, SLOT(slotEnvSetting()));
+    connect(ui_->actionLoadModel, SIGNAL(triggered()), this, SLOT(slotLoadModel()));
+    connect(ui_->actionShowLights, SIGNAL(triggered()), this, SLOT(slotShowLights()));
+    connect(ui_->actionDrawCube, SIGNAL(triggered()), this, SLOT(slotDrawCube()));
+    connect(ui_->actionDrawPlane, SIGNAL(triggered()), this, SLOT(slotDrawPlane()));
+    connect(ui_->actionDepthTest, SIGNAL(triggered()), this, SLOT(slotDepthTestSetting()));
+    connect(ui_->actionDepthTestShader, SIGNAL(triggered()), this, SLOT(slotZbufferShader()));
+    connect(ui_->actionStencil, SIGNAL(triggered()), this, SLOT(slotStenCil()));
+    connect(ui_->actionDrawVegetation, SIGNAL(triggered()), this, SLOT(slotDrawVegetation()));
+    connect(ui_->actionDrawWindows, SIGNAL(triggered()), this, SLOT(slotDrawWindows()));
+    connect(ui_->actionDrawFramBuffer, SIGNAL(triggered()), this, SLOT(slotDrawFramBuffer()));
+    connect(ui_->actionSelectFrambufferType, SIGNAL(triggered()), this, SLOT(slotSelectFramBufferType()));
+    connect(ui_->openGLWidget,&ModelLoadingOWgt::signalMousePickingPos,
+            this, &MainWindow::slotMousePickingPos);
+    connect(ui_->actionOpenSkyBox, SIGNAL(triggered()), this, SLOT(slotOpenSkyBox()));
+    connect(ui_->actionReflectIonSkyBox, SIGNAL(triggered()), this, SLOT(slotReflectionSkyBox()));
+    connect(ui_->actionReflractionSkyBox, SIGNAL(triggered()), this, SLOT(slotRefractionSkyBox()));
+    connect(ui_->actionReflectTexSkyBox, SIGNAL(triggered()), this, SLOT(slotReflectTextureSkyBox()));
+    connect(ui_->actionPoints, SIGNAL(triggered()), this, SLOT(slotPointsView()));
+    connect(ui_->actionDrawHouse, SIGNAL(triggered()), this, SLOT(slotDrawHouse()));
+    connect(ui_->actionDrawExplode, SIGNAL(triggered()), this, SLOT(slotDrawExplode()));
+    connect(ui_->actionshowNormal, SIGNAL(triggered()), this, SLOT(slotShowNormal()));
+    connect(ui_->actionDrawInstancing, SIGNAL(triggered()), this, SLOT(slotDrawInstancing()));
+    connect(ui_->actionDrawPlanet, SIGNAL(triggered()), this, SLOT(slotDrawPlanets()));
+    connect(ui_->actionDrawTriangle, SIGNAL(triggered()), this, SLOT(slotDrawTriangle()));
+}
+
+void MainWindow::slotWireframeView()
+{
+    ui_->openGLWidget->setWireframe(ui_->actionWireFrameView->isChecked());
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui_;
+}
+
+void MainWindow::slotEnvSetting() {
+    auto dlg = new EnvironmentSettingDialog(this);
+    dlg->setEnvType(ui_->openGLWidget->getViewEnvType());
+    if(dlg->exec()==QDialog::Accepted){
+        ui_->openGLWidget->setEnvSettingType(dlg->getEnvType());
+    }
+    delete dlg;
+}
+
+void MainWindow::slotLoadModel() {
+    QString str = QFileDialog::getOpenFileName(this,"选择模型文件","",
+                                             "OBJ (*.obj);;PLY (*.ply);;FBX(*.fbx);;STL (*.stl);;ALL FILES( *.* ) ");
+    ui_->openGLWidget->loadModel(str.toStdString());
+}
+
+void MainWindow::slotShowLights() {
+
+    auto bShowLight = ui_->openGLWidget->isBShowLights();
+    ui_->openGLWidget->setBShowLights(!bShowLight);
+}
+
+void MainWindow::slotDrawCube() {
+    auto bDrawCube = ui_->openGLWidget->isBDrawCube();
+    ui_->openGLWidget->setBDrawCube(!bDrawCube);
+}
+
+void MainWindow::slotDrawPlane() {
+    auto bDrawPlane = ui_->openGLWidget->isBDrawPlane();
+    ui_->openGLWidget->setBDrawPlane(!bDrawPlane);
+}
+
+void MainWindow::slotDepthTestSetting() {
+    auto dlg = new DepthTestDialog(this);
+    dlg->setDepthTestFunc(ui_->openGLWidget->getTestFuncType());
+    if(dlg->exec()==QDialog::Accepted){
+        ui_->openGLWidget->setTestFuncType(dlg->getDepthTestFunc());
+    }
+    delete dlg;
+}
+
+void MainWindow::slotZbufferShader() {
+    auto bZBufferTest = ui_->openGLWidget->isBDepthTestShader();
+    ui_->openGLWidget->setBDepthTestShader(!bZBufferTest);
+}
+
+void MainWindow::slotMousePickingPos(QVector3D pos) {
+    ui_->statusbar->setStyleSheet("font: 14pt ");
+    ui_->statusbar->showMessage(" 世界坐标:    X:"
+                               +QString::number(pos.x(),'f', 2)
+                               +" Y:"+QString::number(pos.y(),'f', 2)
+                               +" Z:"+QString::number(pos.z(),'f', 2)
+    );
+}
+
+void MainWindow::slotStenCil() {
+    auto bStencil = ui_->actionStencil->isChecked();
+    ui_->openGLWidget->setBStencil(bStencil);
+}
+
+void MainWindow::slotDrawVegetation() {
+    auto bDrawVegetation = ui_->openGLWidget->isBDrawVegetation();
+    ui_->openGLWidget->setBDrawVegetation(!bDrawVegetation);
+}
+
+void MainWindow::slotDrawWindows() {
+    auto bDrawWindows = ui_->openGLWidget->isBDrawWindows();
+    ui_->openGLWidget->setBDrawWindows(!bDrawWindows);
+}
+
+void MainWindow::slotDrawFramBuffer() {
+    auto bDrawFramBuffer = ui_->openGLWidget->isBDrawFramBuffer();
+    ui_->openGLWidget->setBDrawFramBuffer(!bDrawFramBuffer);
+}
+
+void MainWindow::slotSelectFramBufferType() {
+    auto dlg = new FrambufferDialog(this);
+    dlg->setFbType(ui_->openGLWidget->getFbType());
+    if(dlg->exec()==QDialog::Accepted){
+        ui_->openGLWidget->setFbtSettingType(dlg->getFbType());
+    }
+    delete dlg;
+}
+
+void MainWindow::slotOpenSkyBox() {
+    ui_->openGLWidget->setBOpenSkyBox(ui_->actionOpenSkyBox->isChecked());
+}
+
+void MainWindow::slotReflectionSkyBox() {
+    ui_->actionReflractionSkyBox->setChecked(false);
+    ui_->actionReflectTexSkyBox->setChecked(false);
+    ui_->openGLWidget->setBReflectionSkyBox(ui_->actionReflectIonSkyBox->isChecked());
+}
+
+void MainWindow::slotRefractionSkyBox() {
+    ui_->actionReflectIonSkyBox->setChecked(false);
+    ui_->actionReflectTexSkyBox->setChecked(false);
+    ui_->openGLWidget->setBRefractionSkyBox(ui_->actionReflractionSkyBox->isChecked());
+}
+
+void MainWindow::slotReflectTextureSkyBox() {
+    ui_->actionReflectIonSkyBox->setChecked(false);
+    ui_->actionReflractionSkyBox->setChecked(false);
+    ui_->openGLWidget->setBReflectTexturteSkyBox(ui_->actionReflectTexSkyBox->isChecked());
+}
+
+void MainWindow::slotPointsView() {
+    ui_->openGLWidget->setPointRender(!ui_->openGLWidget->isBPoints());
+}
+
+void MainWindow::slotDrawHouse() {
+    auto bDrawHouse = ui_->openGLWidget->isBDrawHouse();
+    ui_->openGLWidget->setBDrawHouse(!bDrawHouse);
+}
+
+void MainWindow::slotDrawExplode() {
+    auto bDrawExplode = ui_->openGLWidget->isBDrawExplode();
+    ui_->openGLWidget->setBDrawExplode(!bDrawExplode);
+}
+
+void MainWindow::slotShowNormal() {
+    ui_->openGLWidget->setShowNormalDgVisible(ui_->actionshowNormal->isChecked());
+}
+
+void MainWindow::slotDrawInstancing() {
+    bool bDrawInstancing = ui_->openGLWidget->isBDrawInstancing();
+    ui_->openGLWidget->setBDrawInstancing(!bDrawInstancing);
+}
+
+void MainWindow::slotDrawPlanets() {
+    bool bDrawPlanets = ui_->openGLWidget->isBDrawPlanets();
+    ui_->openGLWidget->setBDrawPlanets(!bDrawPlanets);
+}
+
+void MainWindow::slotDrawTriangle() {
+    bool bDrawTriangle = ui_->openGLWidget->isBDrawTriangle();
+    ui_->openGLWidget->setBDrawTriangle(!bDrawTriangle);
+}
---
+++ QtOpengl/33_01_AntiAliasing/shapes.frag
@@ -0,0 +1,213 @@
+/// shapes.frag
+//#version 120
+
+//varying vec3 v_normal;
+//varying vec3 v_vertex;
+//varying vec2 v_texcoord;
+
+#version 410 core
+
+in VS_OUT
+{
+    vec3 v_normal;
+    vec3 v_vertex;
+    vec2 v_texcoord;
+
+}fs_in;
+
+out vec4 FragColor;
+
+struct Material {
+    sampler2D texture_diffuse1;
+    sampler2D texture_specular1;
+    sampler2D texture_emission1;
+    sampler2D texture_reflection1;
+    float shininess;
+};
+
+struct SPotLight {
+    vec3 position;
+    vec3 direction;
+    float cutOff;
+    float outerCutOff;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+
+    float constant;
+    float linear;
+    float quadratic;
+};
+
+struct DirLight {
+    vec3 direction;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+struct PointLight {
+    vec3 position;
+
+    float constant;
+    float linear;
+    float quadratic;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+#define NR_POINT_LIGHTS 4
+
+uniform SPotLight spotLight;
+uniform DirLight dirLight;
+uniform PointLight pointLights[NR_POINT_LIGHTS];
+
+uniform Material material;
+
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+uniform bool bSimpleZBudder;
+uniform bool bReflectTexturteSkyBox;
+
+//vec3 diffuseTexColor = vec3(texture2D(material.diffuse, v_texcoord));
+//vec3 specularTexColor = vec3(texture2D(material.specular, v_texcoord));
+
+vec3 diffuseTexColor = vec3(texture(material.texture_diffuse1, fs_in.v_texcoord));
+vec3 specularTexColor = vec3(texture(material.texture_specular1, fs_in.v_texcoord));
+vec3 reflectionTexColor = vec3(texture(material.texture_reflection1, fs_in.v_texcoord));
+float near = 0.1;
+float far = 100.0;
+
+vec3 CalcSpotLight(SPotLight light, vec3 normal, vec3 viewDir);
+vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir);
+vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
+
+float LinearizeDepth(float depth);
+
+uniform samplerCube skybox;
+
+void main()
+{
+    vec3 norm = normalize(fs_in.v_normal);
+    vec3 viewDir = normalize(viewPos - fs_in.v_vertex);
+    vec3 reflectDir = reflect(-viewDir, norm);
+
+    vec3 result = vec3(0);
+    result += CalcSpotLight(spotLight, norm, viewDir);
+    result += CalcDirLight(dirLight, norm, viewDir);
+
+    for (int i=0; i<NR_POINT_LIGHTS; i++)
+    result += CalcPointLight(pointLights[i], norm, fs_in.v_vertex, viewDir);
+
+    /// reflection
+    vec3 reflectionSkyBox = texture(skybox, reflectDir).rgb;
+    vec3 reflection= reflectionTexColor * reflectionSkyBox;
+    if (bReflectTexturteSkyBox)
+    {
+        /// 可以用于处理 渲染左右两个不一样的渲染效果
+        if (gl_FragCoord.x > 320)
+            result += reflection;
+    }
+
+
+    if (!bSimpleZBudder)
+    {
+        FragColor = vec4(result, 1.0);
+        FragColor = vec4(result, 1.0);
+    }
+    else
+    {
+        float depth = (LinearizeDepth(gl_FragCoord.z)-near) / (far-near); // divide by far for demonstration
+        FragColor = vec4(vec3(depth), 1.0);
+    }
+
+
+}
+
+vec3 CalcSpotLight(SPotLight light, vec3 normal, vec3 viewDir)
+{
+    /// ambient
+    vec3 ambient = diffuseTexColor * light.ambient;
+
+    /// diffuse
+    vec3 lightDir = normalize(light.position - fs_in.v_vertex);
+    float diff = max(dot(normal, lightDir), 0.0);
+    vec3 diffuse = diff * diffuseTexColor * light.diffuse;
+
+    /// specular
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular =  spec*specularTexColor * light.specular;
+
+    //attenuation
+    float distance = length(light.position - fs_in.v_vertex);
+    float attenuation = 1.0 / (light.constant + light.linear * distance +
+    light.quadratic * (distance * distance));
+    //ambient *= attenuation;
+    diffuse *= attenuation;
+    specular *= attenuation;
+
+    //smooth
+    float theta = dot(lightDir, normalize(-light.direction));
+    float epsilon = light.cutOff - light.outerCutOff;
+    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
+
+    diffuse *= intensity;
+    specular *= intensity;
+
+    return (ambient + diffuse + specular);
+}
+
+vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir)
+{
+    // diffuse shading
+    vec3 lightDir = normalize(-light.direction);
+    float diff = max(dot(normal, lightDir), 0.0);
+
+    // specular shading
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+
+    // combine results
+    vec3 ambient = light.ambient * diffuseTexColor;
+    vec3 diffuse = light.diffuse * diff * diffuseTexColor;
+    vec3 specular = light.specular * spec * specularTexColor;
+    return (ambient + diffuse + specular);
+}
+
+vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
+{
+    // diffuse shading
+    vec3 lightDir = normalize(light.position - fragPos);
+    float diff = max(dot(normal, lightDir), 0.0);
+
+    // specular shading
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+
+    // attenuation
+    float distance = length(light.position - fragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+
+
+    vec3 ambient = light.ambient * vec3(texture(material.texture_diffuse1, fs_in.v_texcoord));
+    vec3 diffuse = light.diffuse * diff * vec3(texture(material.texture_diffuse1, fs_in.v_texcoord));
+    vec3 specular = light.specular * spec * vec3(texture(material.texture_specular1, fs_in.v_texcoord));
+
+    ambient *= attenuation;
+    diffuse *= attenuation;
+    specular *= attenuation;
+
+    return (ambient + diffuse + specular);
+}
+
+float LinearizeDepth(float depth) {
+    float z_ndc = depth * 2.0 - 1.0; // back to NDC
+    return (2.0 * near * far) / (far + near - z_ndc  * (far - near));
+}
+
